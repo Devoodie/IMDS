1ed7dc2d331d86e436047d8407be4f2a42099942
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-05-25 00:58:34 -0400

    Currently doing rapidifre ommit test there is a problem with omit. once done need to finish input, then it is deployable

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Session:
             pass
 
 
-def jobcloser(htmlele, tonumber, *args, rapidfire=False):
+def jobcloser(htmlele, tonumber=123456789123456789, *args, rapidfire=False, omit=None):
     windows = driver.window_handles
     wait.until(EC.element_to_be_clickable(htmlele))
     element = htmlele
@@ -117,88 +117,94 @@ def jobcloser(htmlele, tonumber, *args, rapidfire=False):
                 driver.switch_to.window(windows[1])
                 return job
         elif rapidfire is True:
-            if "STARTED" in element.text:
-                starting_point = element.text.find("STARTED") + 8
+            if omit is not None:
+                for TO in omit:
+                    if f'-{TO}' in element.text:
+                        print(f"TO ommited: {TO}")
+                        return "a"
             else:
-                starting_point = element.text.find("SCHEDULED") + 10
-            iterable = 0
-            while True:
-                character = element.text[starting_point + iterable]
-                if character in string.digits:
-                    iterable += 1
+                if "STARTED" in element.text:
+                    starting_point = element.text.find("STARTED") + 8
                 else:
-                    jobidee = iterable + starting_point
-                    job = element.text[starting_point:jobidee]
-                    break
-            driver.close()
-            windows = driver.window_handles
-            driver.switch_to.window(windows[0])
-            wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div[1]/iframe[2]')))
-            try:  # time start
-                driver.find_element(By.NAME, 'R7_C29_L4_N').is_displayed()
-                driver.find_element(By.NAME, 'R7_C29_L4_N').send_keys(Timestart)
-            except NoSuchElementException:
-                wait.until(EC.element_to_be_clickable((By.NAME, 'R9_C33_L4_N')))
-                driver.find_element(By.NAME, 'R9_C33_L4_N').send_keys(Timestart)
-            try:  # time end
-                driver.find_element(By.NAME, 'R9_C45_L4_N').send_keys(Timeend)
-            except NoSuchElementException:
-                driver.find_element(By.NAME, 'R7_C42_L4_N').send_keys(Timeend)
-            try:  # type maintenance # type maintenance
-                element = driver.find_element(By.NAME, 'R12_C18_L60_N')
-                elementval = element.get_property("value")
-                if "PRE-DEPLOYMENT INSPECTION" in elementval or "POST DEPLOYMENT INSPECTION" in elementval:
-                    driver.find_element(By.NAME, 'R7_C1_L1_N').send_keys("S")
-                else:
-                    driver.find_element(By.NAME, 'R7_C1_L1_N').send_keys("T")
-            except NoSuchElementException:
-                pass
-            driver.find_element(By.NAME, 'cs').send_keys("2")  # crew size
-            try:  # units produced
-                driver.find_element(By.NAME, 'R7_C25_L2_N').send_keys("1")
-            except NoSuchElementException:
-                driver.find_element(By.NAME, 'R9_C30_L2_N').send_keys("1")
-            driver.find_element(By.NAME, 'cat').send_keys("1")  # category
-            try:  # corrected by
-                if driver.find_element(By.NAME, 'R7_C62_L12_N').is_enabled():
-                    driver.find_element(By.NAME, 'R7_C62_L12_N').send_keys(FriendID)
-            except NoSuchElementException:
-                if driver.find_element(By.NAME, 'R9_C61_L12_N').is_enabled():
-                    driver.find_element(By.NAME, 'R9_C61_L12_N').send_keys(FriendID)
-            try:  # inspected by
-                driver.find_element(By.NAME, 'R19_C43_L12_N').send_keys(UserID)
-            except NoSuchElementException:
-                driver.find_element(By.NAME, 'R9_C22_L12_N').send_keys(UserID)
-            element = driver.find_element(By.NAME, 'CANarrativeTextarea')  # corrective action
-            element.clear()
-            element.send_keys(CorrectiveAction)
-            try:
-                driver.find_element(By.NAME, 'rad1').click()
-            except NoSuchElementException:
-                pass
-            driver.switch_to.default_content()
-            try:  # see if error submitting
-                if len(jobsfailed) == 0:
-                    driver.find_element(By.CLASS_NAME, ("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable"))
-                    element = driver.find_elements(By.CLASS_NAME, (("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable")))
-                    jobsfailed.append(job)
-                    print(f'Job failed: {job}')
-                    windows = driver.window_handles
-                    driver.switch_to.window(windows[1])
-                    return job
-                else:
-                    driver.find_element(By.CLASS_NAME, ("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable"))
-                    element = driver.find_elements(By.CLASS_NAME, (("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable")))
-                    driver.find_element(element[len(jobsfailed)-1])
-                    jobsfailed.append(job)
-                    print(f'Job failed: {job}')
-                    windows = driver.window_handles
-                    driver.switch_to.window(windows[1])
-                    return job
-            except (NoSuchElementException, IndexError):
+                    starting_point = element.text.find("SCHEDULED") + 10
+                iterable = 0
+                while True:
+                    character = element.text[starting_point + iterable]
+                    if character in string.digits:
+                        iterable += 1
+                    else:
+                        jobidee = iterable + starting_point
+                        job = element.text[starting_point:jobidee]
+                        break
+                driver.close()
                 windows = driver.window_handles
-                driver.switch_to.window(windows[1])
-                return job
+                driver.switch_to.window(windows[0])
+                wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div[1]/iframe[2]')))
+                try:  # time start
+                    driver.find_element(By.NAME, 'R7_C29_L4_N').is_displayed()
+                    driver.find_element(By.NAME, 'R7_C29_L4_N').send_keys(Timestart)
+                except NoSuchElementException:
+                    wait.until(EC.element_to_be_clickable((By.NAME, 'R9_C33_L4_N')))
+                    driver.find_element(By.NAME, 'R9_C33_L4_N').send_keys(Timestart)
+                try:  # time end
+                    driver.find_element(By.NAME, 'R9_C45_L4_N').send_keys(Timeend)
+                except NoSuchElementException:
+                    driver.find_element(By.NAME, 'R7_C42_L4_N').send_keys(Timeend)
+                try:  # type maintenance # type maintenance
+                    element = driver.find_element(By.NAME, 'R12_C18_L60_N')
+                    elementval = element.get_property("value")
+                    if "PRE-DEPLOYMENT INSPECTION" in elementval or "POST DEPLOYMENT INSPECTION" in elementval:
+                        driver.find_element(By.NAME, 'R7_C1_L1_N').send_keys("S")
+                    else:
+                        driver.find_element(By.NAME, 'R7_C1_L1_N').send_keys("T")
+                except NoSuchElementException:
+                    pass
+                driver.find_element(By.NAME, 'cs').send_keys("2")  # crew size
+                try:  # units produced
+                    driver.find_element(By.NAME, 'R7_C25_L2_N').send_keys("1")
+                except NoSuchElementException:
+                    driver.find_element(By.NAME, 'R9_C30_L2_N').send_keys("1")
+                driver.find_element(By.NAME, 'cat').send_keys("1")  # category
+                try:  # corrected by
+                    if driver.find_element(By.NAME, 'R7_C62_L12_N').is_enabled():
+                        driver.find_element(By.NAME, 'R7_C62_L12_N').send_keys(FriendID)
+                except NoSuchElementException:
+                    if driver.find_element(By.NAME, 'R9_C61_L12_N').is_enabled():
+                        driver.find_element(By.NAME, 'R9_C61_L12_N').send_keys(FriendID)
+                try:  # inspected by
+                    driver.find_element(By.NAME, 'R19_C43_L12_N').send_keys(UserID)
+                except NoSuchElementException:
+                    driver.find_element(By.NAME, 'R9_C22_L12_N').send_keys(UserID)
+                element = driver.find_element(By.NAME, 'CANarrativeTextarea')  # corrective action
+                element.clear()
+                element.send_keys(CorrectiveAction)
+                try:
+                    driver.find_element(By.NAME, 'rad1').click()
+                except NoSuchElementException:
+                    pass
+                driver.switch_to.default_content()
+                try:  # see if error submitting
+                    if len(jobsfailed) == 0:
+                        driver.find_element(By.CLASS_NAME, ("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable"))
+                        element = driver.find_elements(By.CLASS_NAME, (("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable")))
+                        jobsfailed.append(job)
+                        print(f'Job failed: {job}')
+                        windows = driver.window_handles
+                        driver.switch_to.window(windows[1])
+                        return job
+                    else:
+                        driver.find_element(By.CLASS_NAME, ("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable"))
+                        element = driver.find_elements(By.CLASS_NAME, (("ui-dialog imdsalert ui-widget ui-widget-content ui-front ui-dialog-buttons ui-draggable")))
+                        driver.find_element(element[len(jobsfailed)-1])
+                        jobsfailed.append(job)
+                        print(f'Job failed: {job}')
+                        windows = driver.window_handles
+                        driver.switch_to.window(windows[1])
+                        return job
+                except (NoSuchElementException, IndexError):
+                    windows = driver.window_handles
+                    driver.switch_to.window(windows[1])
+                    return job
         else:
             driver.close()
             windows = driver.window_handles
@@ -258,10 +264,10 @@ def main():
     for count, eluhment in enumerate(iterjobs):
         if count == 0:
             continue
-        if iterations >= len(jobstoautomate):
+        if iterations >= len(iterjobs)-1:
             break
         else:
-            placeholderstring = jobcloser(eluhment, jobstoautomate[iterations])
+            placeholderstring = jobcloser(eluhment, rapidfire=True)
             if placeholderstring.isdigit():
                 jobids.append(placeholderstring)
                 iterations += 1
